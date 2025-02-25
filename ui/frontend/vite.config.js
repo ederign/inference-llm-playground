84c64071238719f20bcb1b3d4cb17d359e31d26d
Eder Ignatowicz <ignatowicz@gmail.com>
2025-02-25 11:20:10 -0500

    final version?

--- ui/frontend/vite.config.js
+++ ui/frontend/vite.config.js
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsInlineLimit: Infinity,
+    cssCodeSplit: false,
+    chunkSizeWarningLimit: 10000,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/',
 })
